961b9a99f3fb1f89cdf9091e62f79143ff57b720
Factor101 <61631765+Factor101@users.noreply.github.com>
2025-02-26 16:09:43 -0500

    Beggining implementation

--- CYB236_Cipher/CryptoAlgo.cpp
+++ CYB236_Cipher/CryptoAlgo.cpp
@@ -4,31 +4,126 @@
 
 #include "rijindael_sbox.h"
 
-uint8_t* CryptoAlgo::padPlaintext(const uint8_t* msg, size_t msgSizeBytes)
+std::pair<uint8_t*, size_t> CryptoAlgo::padPlaintext(const uint8_t* msg, const size_t msgSizeBytes)
 {
-	// ensure msg bytes % (BLOCK_SIZE_BITS / 8) == 0, and msgSizeBytes >= BLOCK_SIZE_BITS / 8
-	if (msgSizeBytes % (BLOCK_SIZE_BITS / 8) == 0)
-	{
-		return (uint8_t*)msg;
-	}
+	printf("Original Plaintext (%llu bytes): ", msgSizeBytes);
+	CryptoAlgo::printMsgBytes(msg, msgSizeBytes);
 
-	// pad msg with 0s
+	// Pad the plaintext to a multiple of the block size
+	const auto sizeBits = msgSizeBytes * 8;
+	const size_t paddedSizeBytes = sizeBits % BLOCK_SIZE_BITS == 0
+		? msgSizeBytes
+		: (msgSizeBytes + (BLOCK_SIZE_BITS - sizeBits % BLOCK_SIZE_BITS) / 8);
 
+	// Allocate the padded message and copy the original message into it
+	uint8_t* paddedMsg = new uint8_t[paddedSizeBytes];
+	memcpy(paddedMsg, msg, msgSizeBytes);
+	memset(paddedMsg + msgSizeBytes, 0, paddedSizeBytes - msgSizeBytes);
+
+	printf("Padding plaintext from %zu bytes to %zu bytes\n", msgSizeBytes, paddedSizeBytes);
+	std::cout << "Padded plaintext (bytes): ";
+	CryptoAlgo::printMsgBytes(paddedMsg, paddedSizeBytes);
+
+	// Return the padded message and its new size
+	return std::make_pair(paddedMsg, paddedSizeBytes);
 }
 
 uint8_t* CryptoAlgo::encrypt(const uint8_t* msg, size_t msgSizeBytes, uint32_t initialKey)
 {
-	msg = CryptoAlgo::padPlaintext(msg, msgSizeBytes);
-	msgSizeBytes = strlen((const char*)msg);
+	printf("Encrypting plaintext of %zu bytes\n", msgSizeBytes);
 
-	std::cout << "msg: " << msg << std::endl;
-	std::cout << "msg size (bytes): " << msgSizeBytes << std::endl;
+	// Pad plaintext and get new msg pointer and size
+	const std::pair<uint8_t*, size_t> paddedState = CryptoAlgo::padPlaintext(msg, msgSizeBytes);
+	uint8_t* state = paddedState.first;
+	size_t paddedStateSize = paddedState.second;
+
+	// we should perform initial permutation here but not defined
+
 
 
 	return nullptr;
+}
+
+void CryptoAlgo::encryptBlock(uint8_t* blockStart, const uint32_t key)
+{
+	// Split the block into left and right halves of BLOCK_SIZE / 2
+	constexpr auto HALF_BLOCK_SIZE_BYTES = BLOCK_SIZE_BITS / 2ul;
+	uint8_t* leftBlock = blockStart;
+	uint8_t* rightBlock = blockStart + HALF_BLOCK_SIZE_BYTES;
+
+	// XOR right block with key
+	// 32b key is split into 4 8b bytes
+	CryptoAlgo::xorBytesWithKey(rightBlock, HALF_BLOCK_SIZE_BYTES, key);
+
+	// diagram says expand to 24 bits but then after SBOX we have 16 bits again
+	// so I am ignoring that step
+
+	// Apply S-Box to right block
+	CryptoAlgo::applySBox(rightBlock, HALF_BLOCK_SIZE_BYTES);
+
+	// apply "permutation (fp)" (undefined) to right block
+
+	// XOR left block with right block
+	CryptoAlgo::xorBytes(leftBlock, HALF_BLOCK_SIZE_BYTES, rightBlock);
+
+	// swap, then write left and right blocks back into blockStart
+
+	// Allocate a tmp buffer to hold one half of the block during the swap
+	uint8_t* tmp = (uint8_t*)malloc(HALF_BLOCK_SIZE_BYTES);
+	if (tmp == nullptr)
+	{
+		std::cerr << "Failed to allocate memory for block swap!" << std::endl;
+		exit(-1);
+	}
+
+	// Swap left and right blocks
+	// Copy left into tmp
+	memcpy(tmp, leftBlock, HALF_BLOCK_SIZE_BYTES);
+	memcpy(leftBlock, rightBlock, HALF_BLOCK_SIZE_BYTES);
+	memcpy(rightBlock, tmp, HALF_BLOCK_SIZE_BYTES);
+
+	// Free tmp buffer
+	free(tmp);
+}
+
+void CryptoAlgo::xorBytes(uint8_t* bytes, const size_t nBytes, const uint8_t* operandBytes)
+{
+	for(size_t i = 0; i < nBytes; ++i)
+	{
+		bytes[i] ^= operandBytes[i];
+	}
+}
+
+void CryptoAlgo::xorBytesWithKey(uint8_t* bytes, const size_t nBytes, const uint32_t key)
+{
+	// XOR each byte w/ the corresponding byte of the key
+	// Key is right shifted by (i * 8) bits to get the correct byte
+	// Finally mask (& 0xFF) to get only the last 8 bits of the 32b key
+	for (size_t i = 0; i < nBytes; ++i)
+	{
+		bytes[i] ^= (key >> (i * 8)) & 0xFF;
+	}
+}
+
+void CryptoAlgo::applySBox(uint8_t* bytes, const size_t nBytes)
+{
+	// Apply S-Box to each byte of the right block
+	for(size_t i = 0; i < nBytes; ++i)
+	{
+		bytes[i] = SBOX[bytes[i]];
+	}
 }
 
 uint8_t* CryptoAlgo::decrypt(const uint8_t* msg, size_t msgSizeBytes, uint32_t initialKey)
 {
 	return nullptr;
+}
+
+void CryptoAlgo::printMsgBytes(const uint8_t* msg, const size_t msgSizeBytes)
+{
+	for(size_t i = 0; i < msgSizeBytes; ++i)
+	{
+		std::cout << std::hex << (int)msg[i] << " ";
+	}
+	std::cout << std::endl << std::dec;
 }
--- CYB236_Cipher/CryptoAlgo.h
+++ CYB236_Cipher/CryptoAlgo.h
@@ -6,8 +6,14 @@ class CryptoAlgo
 {
 private:
 	constexpr static uint32_t BLOCK_SIZE_BITS = 32;
-	static uint8_t* padPlaintext(const uint8_t* msg, size_t msgSizeBytes);
+	constexpr static uint32_t BLOCK_SIZE_BYTES = BLOCK_SIZE_BITS / 4ul;
+	static std::pair<uint8_t*, size_t> padPlaintext(const uint8_t* msg, const size_t msgSizeBytes);
+	static void encryptBlock(uint8_t* blockStart, const uint32_t key);
+	static void xorBytes(uint8_t* bytes, const size_t nBytes, const uint8_t* operandBytes);
+	static void xorBytesWithKey(uint8_t* bytes, const size_t nBytes, const uint32_t key);
+	static void applySBox(uint8_t* bytes, const size_t nBytes);
+	static void printMsgBytes(const uint8_t* msg, const size_t msgSizeBytes);
 public:
-	static uint8_t* encrypt(const uint8_t* msg, size_t msgSizeBytes, uint32_t initialKey);
-	static uint8_t* decrypt(const uint8_t* msg, size_t msgSizeBytes, uint32_t initialKey);
+	static uint8_t* encrypt(const uint8_t* msg, const size_t msgSizeBytes, uint32_t initialKey);
+	static uint8_t* decrypt(const uint8_t* msg, const size_t msgSizeBytes, uint32_t initialKey);
 };
